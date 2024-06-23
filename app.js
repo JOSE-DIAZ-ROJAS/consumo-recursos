9b1a21fddc8b0739e2d7cf19041a365ed13e8b0a
AT <jpacedo1@gmail.com>
2024-06-23 01:12:47 -0500

    app.js optiizacion al agregar elementos y formateo de codigo

--- app.js
+++ app.js
@@ -14,41 +14,37 @@ let description = [];
 //[FUNCIONES]
 
 
-async function getData(){ // indicamos que se trata de una funcion asincrona(devolvera una promesa)
-    
-    try{
-const result = await fetch('https://tienda-ropa-1-default-rtdb.firebaseio.com/products.json'); // fetch devuelve promesa, se usa para realizar solicitudes http
+async function getData() { // indicamos que se trata de una funcion asincrona(devolvera una promesa)
 
-if(!result.ok){
-throw new Error(`Error 1: ${result.statusText}`); // statusText nos devuelve le text del codigo de estado
-    }
-const data = await result.json();
-let valores = Object.values(data);
-valores.forEach((element)=>{
+    try {
+        const result = await fetch('https://tienda-ropa-1-default-rtdb.firebaseio.com/products.json'); // fetch devuelve promesa, se usa para realizar solicitudes http
 
+        if (!result.ok) {
+            throw new Error(`Error 1: ${result.statusText}`); // statusText nos devuelve le text del codigo de estado
+        }
+        const data = await result.json();
+        let valores = Object.values(data);
+        valores.forEach((element) => {
 
-imagenes.push(element.fotoUrl);
-description.push(element.descripcion);
+            imagenes.push(element.fotoUrl);
+            description.push(element.descripcion);
 
-
-});
+        });
 
 
-for(i=0;i<imagenes.length;i++){
+        for (i = 0; i < imagenes.length; i++) {
 
-    const pintarimg = document.createElement('div');
-    pintarimg.classList.add('main__item');
-    pintarimg.innerHTML =`
-    
-    <img  class="main__img" src="${imagenes[i]}"/>
-    <p class ="main__text">${description[i]}</p>
-    
-    `;
-    cargarImagen.appendChild(pintarimg);
-};
+            const pintarimg = document.createElement('div');
+            pintarimg.classList.add('main__item');
+            pintarimg.innerHTML = `
+                <img  class="main__img" src="${imagenes[i]}"/>
+                <p class ="main__text">${description[i]}</p>
+            `;
+            cargarImagen.appendChild(pintarimg);
+        };
 
-}catch(error){
-console.log(`Error al obtener la data: ${error}`)
+    } catch (error) {
+        console.log(`Error al obtener la data: ${error}`)
     };
 
 };
